cb8bcf16f8636db527f6af4934f6eb4198709e12
vinson <phenomx49850@gmail.com>
2016-05-28 12:47:44 -0700

    Add files

---
+++ cm.mk
@@ -0,0 +1,18 @@
+
+# Inherit device configuration
+$(call inherit-product, device/zte/warp4/warp4.mk)
+
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 720
+TARGET_SCREEN_WIDTH := 1280
+TARGET_BOOTANIMATION_NAME := 720
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := warp4
+PRODUCT_NAME := cm_warp4
+PRODUCT_BRAND := ZTE
+PRODUCT_MODEL := N9515
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_RELEASE_NAME := Warp Sync
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo cm_warp4-userdebug
