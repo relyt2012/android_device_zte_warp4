54b898d5307c7cfe5197e28ffdb921de61607fa1
Bcrichster <Bcrichster@users.noreply.github.com>
2017-02-11 20:53:53 -0500

    Add files via upload

---
+++ libshim/Android.mk
@@ -0,0 +1,36 @@
+LOCAL_PATH := $(call my-dir)
+
+# libqc-opt
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+     icu4c.c
+
+LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
+LOCAL_MODULE := libshim_qc-opt
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+# rmt_storage
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+     ioprio.c
+
+LOCAL_MODULE := libshim_rmt_storage
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+# libril
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+     libril.c
+
+LOCAL_MODULE := libshim_ril
+LOCAL_SHARED_LIBRARIES := libbinder
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ libshim/libril.c
@@ -0,0 +1,5 @@
+ /* status_t Parcel::writeString16 */
+    int _ZN7android6Parcel13writeString16EPKDsj();
+    int _ZN7android6Parcel13writeString16EPKtj() {
+        return _ZN7android6Parcel13writeString16EPKDsj();
+    }
